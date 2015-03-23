5d02928b626c9b3fbd62b4cec6be169e9852fcec
Cave <caveeagle@gmail.com>
2015-03-23 14:57:56 +0400

    Add dates filter

---
+++ objects_surveys.js
@@ -0,0 +1,13 @@
+
+function INIT_OBJECTS_SURVEYS_TAB()
+{
+  /* Init date fields */
+  document.getElementById("start_objects_date_field").innerHTML = getCurrentYear()+"-01-01";
+  document.getElementById("stop_objects_date_field").innerHTML = today();
+
+    
+}
+
+
+
+
